d1f1a5d2006442ada50484aef3b2d3234b96b868
katiecanada <kredmond@stanford.edu>
2015-05-31 18:56:08 -0700

    changes

--- interactions.js
+++ interactions.js
@@ -32,15 +32,15 @@ d3.json("skills.json", function(error, root) {
 
   var text = svg.selectAll("text")
       .data(nodes)
-    .enter().append("text")
+	  .enter().append("text")
       .attr("class",function(d){return d.type +" label"})
       .style("fill",  "white")
       .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
       .style("display", function(d) { return d.parent === root ? null : "none"; })
       .text(function(d) { return d.name; })
       .attr("startOffset", .25)
-			.attr("text-anchor", "middle")
-      ;
+	  .attr("text-anchor", "middle");
+	
 
   var node = svg.selectAll("circle,text");
 
@@ -75,6 +75,7 @@ d3.json("skills.json", function(error, root) {
     if(d !== root){
 	    d3.selectAll(".navButton").filter(".back").style("display", "inline-block").attr("class", "navButton back "+d.type); 
     }else{
+    	d3.selectAll(".foreign").remove();
     	d3.selectAll(".skill").style("display", "inline");
 	    d3.selectAll(".navButton").filter(".category").style("display", "inline-block"); 
     }
@@ -90,34 +91,36 @@ d3.json("skills.json", function(error, root) {
     	var pie = d3.layout.pie().value(function(d){return d.size;});
 
 		var arc = d3.svg.arc()
-			.innerRadius(diameter/2 - 100)
-			.outerRadius(diameter/2 - 20);
+			.innerRadius(diameter/2 - 90)
+			.outerRadius(diameter/2 - 30);
 		
 		root2 = d.experiences;
    
-		var path = svg.datum(root2).selectAll("path")
+		path = svg.datum(root2).selectAll("path")
 			.data(pie)
 			.enter().append("path")
-			.attr("class", "pie "+d.type)
-			.attr("stroke-width", "1px")
+			.attr("class", function(d,i){return "pie "+ root2[i].number})
+			.classed(d.type, "true")
+			.attr("stroke-width", "0px")
 			.attr("stroke", "white")
+			.attr("opacity",0)
 			.attr("d", function (d) {
 				return arc(d);
 			})
 			.attr("id", function (d,i) {
 				return "path"+i.toString();
-			}); 
+			});
 			
 			
-			 svg.datum(root2).selectAll("label")
+		labels =  svg.datum(root2).selectAll("label")
 			 .data(pie)
 			.enter().append("g")
-			.append("text")
+			.append("text").attr("class","pie_label label "+d.type)
 			.style("font-size", 20)
 			.attr("x", 6)
 			.attr("dy", 50)
-			
 			.append("textPath")
+			.attr("class",function(d,i){return "pie_label label "+root2[i].number})
 			.attr("fill","white")
 			.attr("stroke-width", "none")
 			.attr("letter-spacing", 2)
@@ -126,49 +129,71 @@ d3.json("skills.json", function(error, root) {
 			})
 			.attr("startOffset", .25)
 			.attr("text-anchor", "middle")
-			.attr("class","pie_label label"+d.type)
 			.text(function(d,i) {console.log(root2[i].name); return root2[i].name; })
+			.attr("opacity", 0)
+		
+		descriptions =  svg.datum(root2).selectAll("description")
+			.data(pie)
+			.enter()
+			//.append("g")
+			//.append('rect')
+
+			//.attr('fill', "none")
+			.append("foreignObject")
+			.attr("class", "foreign")
+			.attr('width', diameter/1.8)
+			.attr('height', diameter/1.5)
+			 .attr("startOffset", .25)
+			.attr('x', '-27%')
+			.attr('y', '-10%')
+			//.attr("text-anchor", "middle")
+			.append("xhtml:div")
+			.attr("class",function(d,i){return "description "+root2[i].number})
+			.text(function(d,i) {console.log(root2[i].name); return root2[i].description; })
+		
 			
+		setTimeout(function(){
+			path.transition()
+			.duration(400)
+			.attr("opacity", 1)
+			.attr("stroke-width", "1px")
+
 			
-			/*rotate pie on click From: http://plnkr.co/edit/CmSHEWt1vsUFrRDwuIDL?p=preview*/
-			var curAngle = 0;
-			var interval = null;
-			var isDown = false;
-			var isLeft = false;
-			var lastX = 0;
-			path.on("mousedown", function(d) {
-	        	isDown = true;
-			});
-			path.on("mousemove", function(d) {
-				if (isDown){
-					isLeft = (lastX - d3.event.x < 0);
-					lastX = d3.event.x;
-					if (isLeft){
-           				curAngle += 5;
-		   			}else{
-            			curAngle -= 5;
-					}
-					path.attr("transform", "translate(" + 0 + "," + 0 + ") rotate(" + curAngle + "," + 0 + "," + 0 + ")");
-				}
-			});
-			path.on("mouseup", function(d){
-				isDown = false;
-			});
-			
+			labels.transition()
+			.duration(400)
+			.attr("opacity", 1)
+			}, 350)
+
 			/*rotate pie on click */
-			pi = 3.1415
 			d3.selectAll(".pie")
 			.on("click", function(d) {
-				console.log(d);
-				path.attr("transform", "translate(" + 0 + "," + 0 + ") rotate(" + (d.endAngle+(d.endAngle-d.startAngle)/2)* (180/pi) + "," + 0 + "," + 0 + ")");
-				d3.event.stopPropagation()
-			})
-
+				unselectAll();
+				//d3.select(this).classed("selected", true)
+				rotateTo(d);
+				d3.event.stopPropagation();
+			});
 	}
+		unselectAll();		
+		rotateTo(d3.select(".pie").datum())		
         
   }
 
-  function zoomTo(v) {
+function unselectAll(){
+	d3.selectAll(".selected").classed("selected",false);
+}
+
+function rotateTo(d){
+	pi = 3.1415
+	d3.selectAll("."+d.data.number)
+	.classed("selected", true);
+	endDegrees = d.endAngle*180/pi;
+	startDegrees = d.startAngle*180/pi
+	rotateAngle = 0 - startDegrees - (endDegrees - startDegrees)/2;
+	console.log(rotateAngle);
+	path.transition().attr("transform", "translate(" + 0 + "," + 0 + ") rotate(" + rotateAngle  + "," + 0 + "," + 0 + ")");
+	}
+
+function zoomTo(v) {
     var k = diameter / v[2]; view = v;
     node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
     circle.attr("r", function(d) { return d.r * k; });
